3499007612edd5a4cd86e3a913b89cd975d2fcc6
오지훈 <huni1006@lezhin.com>
2022-12-06 08:17:30 +0900

    Refac HttpResponser

--- http/src/main/java/response/HttpResponser.java
+++ http/src/main/java/response/HttpResponser.java
@@ -1,50 +1,76 @@
 package response;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+import validate.ValidateUtil;
+import static io.IoUtils.createBufferedInputStream;
+import static io.IoUtils.createBufferedOutputStream;
+import static io.IoUtils.createBufferedWriter;
+import static validate.ValidateUtil.validate;
 import static validate.ValidateUtil.validateNull;
 
 public class HttpResponser {
-    private final HttpResponseMessageCreator httpResponseMessageCreator;
+    private static final String END_OF_LINE = "\r\n";
+
+    private final byte[] BUFFER = new byte[8192];
+    private final BufferedWriter bufferedWriter;
     private final BufferedOutputStream bufferedOutputStream;
 
-    public HttpResponser(HttpResponseMessageCreator httpResponseMessageCreator, BufferedOutputStream bufferedOutputStream) {
-        this.httpResponseMessageCreator = validateNull(httpResponseMessageCreator);
-        this.bufferedOutputStream = validateNull(bufferedOutputStream);
+    private String startLine;
+    private String header;
+    private BufferedInputStream bodyInputStream;
+
+    public HttpResponser(OutputStream outputStream) {
+        validateNull(outputStream);
+
+        this.bufferedWriter = createBufferedWriter(outputStream);
+        this.bufferedOutputStream = createBufferedOutputStream(outputStream);
     }
 
     public void send() {
-        doSendHeader();
-        doSendSeparator();
-        doSendBody();
-    }
-
-    private void doSendHeader() {
-        byte[] header = httpResponseMessageCreator.generateHeader();
         try {
-            bufferedOutputStream.write(header);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void doSendSeparator() {
-        byte[] separator = httpResponseMessageCreator.generateSeparator();
-        try {
-            bufferedOutputStream.write(separator);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void doSendBody() {
-        try {
-            while (httpResponseMessageCreator.isLeftContent()) {
-                byte[] content = httpResponseMessageCreator.generateContent();
-                bufferedOutputStream.write(content);
+            if (Objects.nonNull(startLine)) {
+                bufferedWriter.write(startLine);
             }
+            if (Objects.nonNull(header)) {
+                bufferedWriter.write(header);
+            }
+
+            bufferedWriter.write(END_OF_LINE);
+
+            if (Objects.nonNull(bodyInputStream)) {
+                while (bodyInputStream.available() != 0) {
+                    int read = bodyInputStream.read(BUFFER);
+                    bufferedOutputStream.write(BUFFER, 0, read);
+                }
+            }
+
+            bufferedOutputStream.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public HttpResponser startLine(String startLine) {
+        validate(startLine);
+        this.startLine = startLine;
+        return this;
+    }
+
+    public HttpResponser header(String header) {
+        validate(header);
+        this.header = header;
+        return this;
+    }
+
+    public HttpResponser body(InputStream bodyInputStream) {
+        validateNull(bodyInputStream);
+        this.bodyInputStream = createBufferedInputStream(bodyInputStream);
+        return this;
     }
 }

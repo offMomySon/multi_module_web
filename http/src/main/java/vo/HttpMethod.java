5584fd1ceaeaa81297de086902a3c89aaba9a4da
오지훈 <huni1006@lezhin.com>
2022-12-19 21:39:06 +0900

    style: rename_request_response_directory

--- http/src/main/java/vo/HttpMethod.java
+++ http/src/main/java/vo/HttpMethod.java
@@ -1,4 +1,4 @@
-package request;
+package vo;
 
 import java.text.MessageFormat;
 import java.util.Arrays;
--- http/src/test/java/vo/HttpMethodTest.java
+++ http/src/test/java/vo/HttpMethodTest.java
@@ -1,4 +1,4 @@
-package request;
+package vo;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;

384ef12e677158e2ce375733e2a3addbd3e7eb2f
오지훈 <huni1006@lezhin.com>
2022-12-20 23:50:38 +0900

    feat: Impl Marker

---
+++ http/src/main/java/mapper/marker/RequestMapping.java
@@ -0,0 +1,15 @@
+package mapper.marker;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import vo.HttpMethod;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestMapping {
+    String[] value() default {};
+
+    HttpMethod[] method() default {};
+}

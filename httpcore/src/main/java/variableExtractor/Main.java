addc39b1d42d80c4c9844be5b910eac11d08dbd9
ohji100629@naver.com <ohji100629@naver.com>
2023-04-04 00:05:47 +0900

    chore: delete PathVariableParameterConverter

--- httpcore/src/main/java/variableExtractor/Main.java
+++ httpcore/src/main/java/variableExtractor/Main.java
@@ -10,19 +10,16 @@ public class Main {
 
     public static void main(String[] args) {
         String body = "{\"tst\": \"v\"}";
-        Map<String, String> pathVariables = Map.of("k1", "v1", "k2", "v2");
         Map<String, String> queryParams = Map.of("kq1", "vq1", "kq2", "vq2");
 
-        PathVariableParameterConverter pathVariableParamValueExtractor = new PathVariableParameterConverter(new RequestParameters(pathVariables));
         RequestParameterConverter requestParamValueExtractor = new RequestParameterConverter(new RequestParameters(queryParams), new ParamAnnotationValue("k1", true, "defaultValue"));
         RequestBodyParameterConverter requestBodyParamValueExtractor = new RequestBodyParameterConverter(new RequestBodyContent(body));
 
         LastParameterConverter lastParamValueExtractor = new LastParameterConverter(requestBodyParamValueExtractor);
         ChainParameterConverter postChainParamValueExtractor = new ChainParameterConverter(requestParamValueExtractor, lastParamValueExtractor);
-        ChainParameterConverter chainParamValueExtractor = new ChainParameterConverter(pathVariableParamValueExtractor, postChainParamValueExtractor);
 
         Method method = getMethod();
-        MethodParamValueExtractor methodParamValueExtractor = new MethodParamValueExtractor(chainParamValueExtractor, method);
+        MethodParamValueExtractor methodParamValueExtractor = new MethodParamValueExtractor(postChainParamValueExtractor, method);
         Object[] objects = methodParamValueExtractor.extractValues();
 
     }

3597b2ad86e427d167922c9f3bc784ffbb754d95
ohji100629@naver.com <ohji100629@naver.com>
2023-06-08 01:48:43 +0900

    refac: Change Filters base collection map -> list

--- http/src/main/java/filter/Filters.java
+++ http/src/main/java/filter/Filters.java
@@ -1,48 +1,100 @@
 package filter;
 
+import java.text.MessageFormat;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
-import static java.util.function.Function.identity;
+import java.util.stream.Stream;
 
 public class Filters {
-    private final Map<String, Filter> values;
+    private final List<Filter> values;
 
-    public Filters(Map<String, Filter> values) {
+    public Filters(List<Filter> values) {
         Objects.requireNonNull(values);
-
-        this.values = values.entrySet().stream()
-            .filter(e -> Objects.nonNull(e.getKey()))
-            .filter(e -> Objects.nonNull(e.getValue()))
-            .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue, (prev, curr) -> prev));
-    }
-
-    public static Filters from(List<Filter> filters) {
-        Objects.requireNonNull(filters);
-        filters = filters.stream().filter(Objects::nonNull).collect(Collectors.toUnmodifiableList());
-        if (filters.isEmpty()) {
+        values = values.stream().filter(Objects::nonNull).collect(Collectors.toUnmodifiableList());
+        if (values.isEmpty()) {
             throw new RuntimeException("filters is empty.");
         }
 
-        Map<String, Filter> values = filters.stream().collect(Collectors.toUnmodifiableMap(Filter::getName, identity(), (prev, curr) -> prev));
-        return new Filters(values);
+        this.values = values;
     }
 
-    public List<FilterWorker> findMatchFilterWorkers(String requestUrl) {
-        if (Objects.isNull(requestUrl) || requestUrl.isBlank()) {
-            throw new RuntimeException("requestPattern is empty.");
+    public Filters merge(Filters otherFilters) {
+        if (Objects.isNull(otherFilters)) {
+            return this;
         }
 
-        List<String> matchedDistinctFilterNames = values.values().stream()
-            .filter(value -> value.isMatchUrl(requestUrl))
+        Set<String> otherFilterNames = otherFilters.values.stream()
             .map(Filter::getName)
+            .collect(Collectors.toUnmodifiableSet());
+
+        Optional<String> optionalDuplicatFilterName = this.values.stream().map(Filter::getName).filter(otherFilterNames::contains).findAny();
+        if (optionalDuplicatFilterName.isPresent()) {
+            String duplicateFilterName = optionalDuplicatFilterName.get();
+            throw new RuntimeException(MessageFormat.format("Has duplicate filter name. FilterName : `{}`", duplicateFilterName));
+        }
+
+        List<Filter> mergedValues = Stream.of(this.values, otherFilters.values)
+            .flatMap(Collection::stream)
+            .collect(Collectors.toUnmodifiableList());
+        return new Filters(mergedValues);
+    }
+
+    // todo
+    // 받은 입력이 null 이면 excpeiton 이 좋을까?, emtpy 가 좋을까?
+    public List<FilterWorker> findMatchFilterWorkers(String requestUrl) {
+        if (Objects.isNull(requestUrl) || requestUrl.isBlank()) {
+            return Collections.emptyList();
+        }
+
+        List<MatchedFilter> matchedFilters = values.stream()
+            .filter(value -> value.isMatchUrl(requestUrl))
+            .map(MatchedFilter::from)
             .distinct()
             .collect(Collectors.toUnmodifiableList());
 
-        return matchedDistinctFilterNames.stream()
-            .map(values::get)
-            .map(Filter::getFilterWorker)
+        return matchedFilters.stream()
+            .map(MatchedFilter::getFilterWorker)
             .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static class MatchedFilter {
+        private final String filterName;
+        private final FilterWorker filterWorker;
+
+        public MatchedFilter(String filterName, FilterWorker filterWorker) {
+            if (Objects.isNull(filterName) || filterName.isBlank()) {
+                throw new RuntimeException("filterName is empty.");
+            }
+            Objects.requireNonNull(filterWorker);
+
+            this.filterName = filterName;
+            this.filterWorker = filterWorker;
+        }
+
+        public static MatchedFilter from(Filter filter) {
+            Objects.requireNonNull(filter);
+
+            String filterName = filter.getName();
+            FilterWorker filterWorker = filter.getFilterWorker();
+
+            return new MatchedFilter(filterName, filterWorker);
+        }
+
+        public FilterWorker getFilterWorker() {
+            return filterWorker;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MatchedFilter that = (MatchedFilter) o;
+            return Objects.equals(filterName, that.filterName);
+        }
     }
 }

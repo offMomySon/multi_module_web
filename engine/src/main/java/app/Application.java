ccf7f52b699415077e1d5c509835421809781838
오지훈 <huni1006@lezhin.com>
2023-06-19 00:56:46 +0900

    feat: add null check

--- engine/src/main/java/app/Application.java
+++ engine/src/main/java/app/Application.java
@@ -3,12 +3,13 @@ package app;
 import container.ComponentContainerCreator;
 import container.Container;
 import converter.base.CompositeConverter;
-import executor.MethodExecutor;
 import executor.ApplicationRequestExecutor;
+import executor.MethodExecutor;
 import executor.StaticResourceExecutor;
 import filter.ApplicationWebFilterCreator;
 import filter.Filters;
 import java.util.List;
+import java.util.Objects;
 import matcher.ControllerHttpPathMatcherCreator;
 import matcher.HttpPathMatcher;
 import processor.HttpService;
@@ -18,6 +19,14 @@ import resource.ResourceFinder;
 public class Application {
 
     public static void run(Class<?> baseClazz, String basePackage, String resourcePackage) {
+        Objects.requireNonNull(baseClazz);
+        if (Objects.isNull(basePackage) || basePackage.isBlank()) {
+            throw new RuntimeException("basePackage is empty.");
+        }
+        if (Objects.isNull(resourcePackage) || resourcePackage.isBlank()) {
+            throw new RuntimeException("resourcePackage is empty.");
+        }
+
         ClassFinder classFinder = ClassFinder.from(baseClazz, basePackage);
         List<Class<?>> clazzes = classFinder.findClazzes();
         Container container = new ComponentContainerCreator(clazzes).create();

0a9eb5da96681cd88e86e4105b613e3ee93a99a2
오지훈 <huni1006@lezhin.com>
2023-06-18 18:22:12 +0900

    chore: remove does not used code

--- src/main/java/com/main/App.java
+++ src/main/java/com/main/App.java
@@ -8,20 +8,5 @@ import lombok.extern.slf4j.Slf4j;
 public class App {
     public static void main(String[] args) {
         Application.run(App.class, "com.main", "resources");
-//        FileFinder fileFinder = FileFinder.create(App.class, "resources");
-//        Optional<Path> resources = fileFinder.findResource(Path.of("result.txt"));
-//
-//        if (resources.isEmpty()) {
-//            log.info("doesNot exist");
-//        }
-//
-//        Path filePath = resources.get();
-//        log.info(filePath.toString());
-//
-//        try (BufferedOutputStream reader = new BufferedOutputStream(new FileOutputStream(filePath.toFile()))) {
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 }

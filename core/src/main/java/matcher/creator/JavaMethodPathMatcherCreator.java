125fbf4a8f3f7e2f56bef0dfe181b577bfade7e2
오지훈 <huni1006@lezhin.com>
2023-09-03 16:29:58 +0900

    chore: reformat javaMethodPathMatcherCreator

--- core/src/main/java/matcher/creator/JavaMethodPathMatcherCreator.java
+++ core/src/main/java/matcher/creator/JavaMethodPathMatcherCreator.java
@@ -5,6 +5,7 @@ import matcher.JavaMethodEndpointMatcher;
 import matcher.PathUrlMatcher;
 import matcher.RequestMethod;
 import matcher.annotation.RequestMapping;
+import matcher.creator.RequestMappingValueExtractor.RequestMappedMethod;
 import matcher.segment.PathUrl;
 import matcher.segment.SegmentChunkFactory;
 import java.lang.reflect.Method;
@@ -30,7 +31,7 @@ public class JavaMethodPathMatcherCreator {
         List<Method> peekMethods = AnnotationUtils.peekMethods(this.clazz, REQUEST_MAPPING_CLASS).stream()
             .collect(Collectors.toUnmodifiableList());
 
-        List<RequestMappingValueExtractor.RequestMappedMethod> requestMappedMethods = peekMethods.stream()
+        List<RequestMappedMethod> requestMappedMethods = peekMethods.stream()
             .map(valueExtractor::extractRequestMappedMethods)
             .flatMap(List::stream)
             .collect(Collectors.toUnmodifiableList());
@@ -38,12 +39,12 @@ public class JavaMethodPathMatcherCreator {
         return requestMappedMethods.stream()
             .map(requestMappedMethod -> {
                 RequestMethod requestMethod = requestMappedMethod.getRequestMethod();
-                PathUrl baseUrl = PathUrl.from(requestMappedMethod.getUrl());
-                Method javaMethod = requestMappedMethod.getJavaMethod();
 
+                PathUrl baseUrl = PathUrl.from(requestMappedMethod.getUrl());
                 SegmentChunkFactory segmentChunkFactory = new SegmentChunkFactory(baseUrl);
                 PathUrlMatcher pathUrlMatcher = PathUrlMatcher.from(segmentChunkFactory);
 
+                Method javaMethod = requestMappedMethod.getJavaMethod();
                 Class<?> declaringClass = javaMethod.getDeclaringClass();
                 Object declaringInstance = objectRepository.get(declaringClass);
 

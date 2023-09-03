0db1959c03e4ac0d8ae57ea3010725929ddbf341
오지훈 <huni1006@lezhin.com>
2023-09-03 21:39:15 +0900

    chore: delete does not used code.

--- core/src/main/java/matcher/EndpointMatcher.java
+++ core/src/main/java/matcher/EndpointMatcher.java
@@ -7,7 +7,5 @@ import java.util.Optional;
  * matcher 의 매치 여부에 따라 method 를 반환합니다.
  */
 public interface EndpointMatcher {
-//    Optional<MatchedMethod> match(RequestMethod requestMethod, PathUrl requestUrl);
-
     Optional<MatchedEndPoint> match(RequestMethod requestMethod, PathUrl requestUrl);
 }
